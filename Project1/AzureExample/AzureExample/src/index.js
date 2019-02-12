2919df0dfed304856ab8d0123574149083ca42e8
mbockus <bockus@gmail.com>
2019-02-12 10:40:47 -0600

    Bootstrap Grid System and FontAwesome

--- Project1/AzureExample/AzureExample/src/index.js
+++ Project1/AzureExample/AzureExample/src/index.js
@@ -1,7 +1,12 @@
 ﻿import 'bootstrap/dist/css/bootstrap.min.css';
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
 import './scripts/junk';
 import './scripts/junk2';
 import './scripts/junk3';
 import './scripts/junk4';
 import './scripts/junk5';
 import './scripts/junk6';
+
+library.add(faCheck);
+dom.watch();
