bfdd94bfd4f3f473851903ffc8ef6eeb86ec295e
mbockus <bockus@gmail.com>
2019-04-02 08:31:39 -0500

    Added Web API controller for Character resource

---
+++ AngularExample/RickAndMortyApp/RickAndMortyApp/Controllers/Character.cs
@@ -0,0 +1,8 @@
+﻿namespace RickAndMortyApp.Controllers
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AngularExample/RickAndMortyApp/RickAndMortyApp/Controllers/CharacterController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RickAndMortyApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CharacterController : Controller
+    {
+        private Dictionary<int, Character> characters = new Dictionary<int, Character>();
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(characters.Values);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            if(characters.TryGetValue(id, out Character character))
+            {
+                return Ok(character);
+            }
+            return NotFound();
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]Character value)
+        {
+            characters.Add(value.Id, value);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Character value)
+        {
+            characters.Add(id, value);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            characters.Remove(id);
+        }
+    }
+}
